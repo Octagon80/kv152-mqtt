84dfe0bb25fdb077794cdbbc864e6d8cbdc988c1
Octagon80 <octagon80@mail.ru>
2021-09-09 16:31:33 +0500

    Название топиков вынес в конфигурационный файл

--- web-mqtt.php
+++ web-mqtt.php
@@ -15,7 +15,7 @@ define('CLIENT_ID', "web-mqtt");
 //include_once(__DIR__ . "/inc/php_serial.class.php");
 include __DIR__."/inc/mqtt_serv.inc.php";
 $PAUSE = 60; //пауза между запросами
-$MQTT_INFOPANEL_WATCHDOG = 'sysinfo/watchdog/web-mqtt';
+
 //Данные для web
 $webData = array();
 
@@ -57,6 +57,9 @@ function str2float($str) {
 function mqttProcessMesssage($message) {
     global $client;
     global $webData;
+    global $MQTT_METEO_TOPICS_WEATHER;
+    global $MQTT_METEO_TOPICS_YANDEX;
+    
     if (strpos($message->topic, 'weather') !== false)
         echo $message->topic, " ", $message->payload, "\n";
 
@@ -64,72 +67,72 @@ function mqttProcessMesssage($message) {
         $webData['sys']['dtm'] = str2dtm($message->payload);
 
 
-    if ($message->topic == 'weather/winddir/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['WINDDIR_VAL'])
         $webData['winddir']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/winddir/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['WINDDIR_DTM'])
         $webData['winddir']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/wind/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['WIND_VAL'])
         $webData['wind']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/wind/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['WIND_DTM'])
         $webData['wind']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/press/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['PRESS_VAL'])
         $webData['press']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/press/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['PRESS_DTM'])
         $webData['press']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/hum/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['HUM_VAL'])
         $webData['hum']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/hum/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['HUM_DTM'])
         $webData['hum']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/temp/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['TEMP_VAL'])
         $webData['temp']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/temp/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['TEMP_DTM'])
         $webData['temp']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/kpindex/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['KPINDEX_VAL'])
         $webData['kpindex']['value'] = str2int($message->payload);
 
-    if ($message->topic == 'weather/kpindex/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_WEATHER['KPINDEX_DTM'])
         $webData['kpindex']['dtm'] = str2dtm($message->payload);
 
 
-    if ($message->topic == 'weather/yandex/temp/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['TEMP_DTM'])
         $webData['ytemp']['dtm'] = str2dtm($message->payload);
-    if ($message->topic == 'weather/yandex/temp/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['TEMP_VAL'])
         $webData['ytemp']['value'] = str2int($message->payload);
 
 
-    if ($message->topic == 'weather/yandex/press/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['PRESS_VAL'])
         $webData['ypress']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/yandex/press/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['PRESS_DTM'])
         $webData['ypress']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/yandex/wind/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['WINDSPEED_VAL'])
         $webData['ywind']['value'] = str2int($message->payload);
-    if ($message->topic == 'weather/yandex/wind/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['WINDSPEED_DTM'])
         $webData['ywind']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/yandex/winddir/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['WINDDIR_VAL'])
         $webData['ywinddir']['value'] = $message->payload;
-    if ($message->topic == 'weather/yandex/winddir/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['WINDDIR_DTM'])
         $webData['ywinddir']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/yandex/day_name/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['DAYNAME_VAL'])
         $webData['yday_name']['value'] = $message->payload;
-    if ($message->topic == 'weather/yandex/day_name/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['DAYNAME_DTM'])
         $webData['yday_name']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/yandex/type/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['TYPE_VAL'])
         $webData['ytype']['value'] = $message->payload;
-    if ($message->topic == 'weather/yandex/type/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['TYPE_DTM'])
         $webData['ytype']['dtm'] = str2dtm($message->payload);
 
-    if ($message->topic == 'weather/yandex/code/value')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['CODE_VAL'])
         $webData['ycode']['value'] = $message->payload;
-    if ($message->topic == 'weather/yandex/code/dtm')
+    if ($message->topic == $MQTT_METEO_TOPICS_YANDEX['CODE_DTM'])
         $webData['ycode']['dtm'] = str2dtm($message->payload);
 }
 
@@ -160,8 +163,8 @@ while (true) {
      }
     if( $firstTime ){
     echo "Указываем, что требуется принудительное обновления значений всем программам\n";
-      mqtt_write('kv152/pogoda-force-update', '1');        
-      mqtt_write($MQTT_INFOPANEL_WATCHDOG, time());
+      mqtt_write(FORCE_UPDATE, '1');        
+      mqtt_write($MQTT_WEB['WATCHDOG'], time());
     }
     $firstTime = false;
      if (!mqtt_loop()){
@@ -176,7 +179,7 @@ while (true) {
         updateWebData();
         if (!mqtt_loop()) break;
         echo "                   ".($infopanelVisual?"O":"*")."БД\r";
-        mqtt_write($MQTT_INFOPANEL_WATCHDOG, time());
+        mqtt_write($MQTT_WEB['WATCHDOG'], time());
         $infopanelVisual = 1-$infopanelVisual;
        
         if (!sleep_my($PAUSE)) break;
